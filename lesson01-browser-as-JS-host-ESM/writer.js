75ed94a16451e5a2e26d09f823be670fbdd098ed
Miguel Gamboa <miguelgamboa@outlook.com>
2026-09-09 15:01:21 +0100

    Lesson 01 - Browser as JS host; Vite;

---
+++ lesson01-browser-as-JS-host-ESM/writer.js
@@ -0,0 +1,5 @@
+console.log("LOADING module writer.")
+
+export function write(label, msg) {
+    console.log(label + ": " +  msg)
+}
---
+++ lesson01-browser-as-JS-host/add.js
@@ -0,0 +1,15 @@
+// There is NO EXPLICIT dependency of writer.js
+
+function add(a, b) {
+    return a + b
+}
+
+(() => {
+    const pIsel = document.getElementById("pIsel")
+    console.log(pIsel) // output: null => pq?
+
+    const label = "File add"
+
+    write(label, "11 + 13 = " + add(11,13))
+    write(label, "7 + 5 = " + add(7,5))
+})()
